542fe140265a259e6bfc7468d51ca99e18ed3f6c
Jaiganesh <jaigan.pdy@gmail.com>
2024-11-19 21:52:07 +0530

    Create AppModel.swift

---
+++ pilot/pilot/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  pilot
+//
+//  Created by Jaiganesh on 2024-10-29.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
