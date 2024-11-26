0b2b119e24303a32b0b10ac760a6e6511a6927f4
Jaiganesh <jaigan.pdy@gmail.com>
2024-11-26 14:22:44 +0530

    Create AVPlayerView.swift

---
+++ pilot/pilot/AVPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  AVPlayerView.swift
+//  pilot
+//
+//  Created by Jaiganesh on 2024-10-29.
+//
+
+import SwiftUI
+
+struct AVPlayerView: UIViewControllerRepresentable {
+    let viewModel: AVPlayerViewModel
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return viewModel.makePlayerViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // Update the AVPlayerViewController as needed
+    }
+}
