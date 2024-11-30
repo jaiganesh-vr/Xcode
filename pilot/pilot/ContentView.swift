7791ad62a04a3e30f81e77acc5dc2100de010b00
Jaiganesh <jaigan.pdy@gmail.com>
2024-11-30 09:36:17 +0530

    Create ContentView.swift

---
+++ pilot/pilot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  pilot
+//
+//  Created by Jaiganesh on 2024-10-29.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
