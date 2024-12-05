0939542cd0159662b6db3f73292bdf51cd70da08
Jaiganesh <jaigan.pdy@gmail.com>
2024-12-05 10:07:47 +0530

    Create pilotTests.swift

---
+++ pilot/pilotTests/pilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  pilotTests.swift
+//  pilotTests
+//
+//  Created by Jaiganesh on 2024-10-29.
+//
+
+import Testing
+@testable import pilot
+
+struct pilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
