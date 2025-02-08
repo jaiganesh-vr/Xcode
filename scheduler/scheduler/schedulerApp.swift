80f5276b632a23bd1a0c3656aac0fb84f1ab010f
Jaiganesh <jaigan.pdy@gmail.com>
2025-02-08 05:59:25 +0530

    Create schedulerApp.swift

---
+++ scheduler/scheduler/schedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedulerApp.swift
+//  scheduler
+//
+//  Created by Jaiganesh on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct schedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
