51e6a0227a0cb4c954fb839f1df70fee4f229f95
Jaiganesh <jaigan.pdy@gmail.com>
2025-02-09 08:14:49 +0530

    Create schedulerTests.swift

---
+++ scheduler/schedulerTests/schedulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  schedulerTests.swift
+//  schedulerTests
+//
+//  Created by Jaiganesh on 2025-01-25.
+//
+
+import Testing
+@testable import scheduler
+
+struct schedulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
